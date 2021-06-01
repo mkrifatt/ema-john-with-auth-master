dffa9dee4611dee9fe6ce3b89ecd97048db102b6
mkrifatt <kudrat.ullah.rifat@gmail.com>
2021-06-02 02:20:30 +0600

    code pushed

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0t82fSpon-A-WlBZquOV_xGVoNmXpvaU",
+  authDomain: "ema-john-server-6727d.firebaseapp.com",
+  projectId: "ema-john-server-6727d",
+  storageBucket: "ema-john-server-6727d.appspot.com",
+  messagingSenderId: "66277606248",
+  appId: "1:66277606248:web:9357964439625cd1788cdd"
+};
+  export default firebaseConfig;
